0f7be05980818dd5f8432e8352c5f48d08c2076b
CelloCello <cello1124@gmail.com>
2017-04-25 15:41:01 +0800

    part1 integration

---
+++ integration.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+def anonymous(x):
+    return x**2 + 1
+
+
+def integrate(fun, start, end):
+    step = 0.1
+    intercept = start
+    area = 0
+    while intercept < end:
+        intercept += step
+        y = anonymous(intercept)
+        area += step * y
+    return area
+
+print integrate(anonymous, 0, 10)
